921e93d1c40eb7ced96fdcffa3add45cbf758390
Wuggie <kawog17@student.sdu.dk>
2022-09-09 20:50:41 +0200

    color updates and php instead of HTML files

--- Index.php
+++ Index.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Web exercise</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <?php
+            include "serverlogin.php"; 
+          
+          /* change character set to utf8, så vi kan have vores æøå, kilde 
+           * http://php.net/manual/en/mysqli.set-charset.php */
+            if (!$conn->set_charset("utf8")) {
+                printf("Error loading character set utf8: %s\n", $conn->error);
+                exit();
+            }
+        ?>        
         <script src="JS/mainjava.js" type="text/javascript"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
@@ -25,3 +34,6 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </body>
 </html>
+
+
+<!-- To launch this in brackets live preview, url is localhost/*whatever it's called in your "WWW" folder*-->
